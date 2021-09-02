8af0180f83d59e96da2fec9cba10d71a3bc304f3
Michael Rybka <mikerybka@users.noreply.github.com>
2021-09-02 23:58:43 +0000

    add Name and GoFileName to function.go

--- function.go
+++ function.go
@@ -1,3 +1,19 @@
 package types
 
-type Function struct {}
+import "strings"
+
+type Function struct {
+	Name string
+}
+
+func (f *Function) GoFileName() string {
+	validchars := "abcdefghijklmnopqrstuvwxyz"
+	name := ""
+	for _, char := range f.Name {
+		if strings.ContainsRune(validchars, char) {
+			name += string(char)
+		}
+	}
+	name += ".go"
+	return name
+}
