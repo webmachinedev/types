6d88eee7d7b7b03d3720a734c0654389e6440451
Michael Rybka <mikerybka@users.noreply.github.com>
2021-09-03 03:28:08 +0000

    edit function.go

--- function.go
+++ function.go
@@ -4,12 +4,20 @@ import "strings"
 
 type Function struct {
 	Name string
+	Inputs struct {
+		Name string
+		Type string
+	}
+	Outputs struct {
+		Name string
+		Type string
+	}
 }
 
 func (f *Function) GoPackageName() string {
 	validchars := "abcdefghijklmnopqrstuvwxyz"
 	name := ""
-	for _, char := range f.Name {
+	for _, char := range strings.ToLower(f.Name) {
 		if strings.ContainsRune(validchars, char) {
 			name += string(char)
 		}
